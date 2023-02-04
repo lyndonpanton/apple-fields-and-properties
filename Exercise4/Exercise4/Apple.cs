696c6e32a8c4dfcf675acfb7f014d220e18eb8de
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-04 19:03:57 +0000

    Completed the problem
    Supplied requested fields and properties of the apple class

---
+++ Exercise4/Exercise4/Apple.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Apple
+    {
+
+        #region Fields
+        private float amountLeft;
+        private bool organic;
+
+        #endregion
+
+        public float AmountLeft
+        {
+            get
+            {
+                return amountLeft;
+            }
+        }
+
+        #region Properties
+        public bool Organic
+        {
+            get
+            {
+                return organic;
+            }
+        }
+
+        #endregion
+    }
+}
